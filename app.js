a2884fe398ae56c988a6c5f020ec7184f504308d
remi.delima~ <remi.delima@epitech.eu>
2019-11-19 16:39:57 +0100

    home page + dependecy

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cors = require('cors');
+const cookieSession = require('cookie-session');
+const app = express();
+
+const DATABASE_HOST_NAME = 'localhost'
+const DATABASE_NAME = 'playlistDiscovery'
+
+app.use(cors());
+
+//set up view engine
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/images'));
+
+// set session cookie to 24 hours and encrypt cookie
+app.use(cookieSession({
+    maxAge: 24 * 60 * 60 * 1000, // day in millisecondes
+    keys: [keys.session.cookieKey]
+}));
+
+// initialize passport
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.Promise = global.Promise
+
+//connect to mongodb
+mongoose.connect('mongodb://' + DATABASE_HOST_NAME + '/' + DATABASE_NAME, () => {
+    console.log('Connected to Database');
+});
+
+// set up routes
+// app.use('/auth', authRoutes);
+// app.use('/profile', profileRoutes);
+// app.use('/spotify', spotifyRoutes);
+
+//create home route
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+// start server
+app.listen(3000, () => {
+    console.log('app now listening or requests on port 3000');
+});
