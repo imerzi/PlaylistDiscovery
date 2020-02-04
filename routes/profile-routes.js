1dd87d41b95670c14804d022034e739e574a092a
remi.delima~ <remi.delima@epitech.eu>
2020-02-04 21:59:31 +0100

    change design done

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const Playlist = require('../models/user-playlist');
 
 const authCheck = (req, res, next) => {
     if(!req.user) {
@@ -11,7 +12,14 @@ const authCheck = (req, res, next) => {
 };
 
 router.get('/', authCheck, (req, res) => {
-    res.render('profile', {page_name: 'profile', user: req.user});
+    Playlist.find({user: req.user.id}).then((userPlaylists) => {
+        if (userPlaylists) {
+            res.render('profile', {page_name: 'profile', user: req.user, playlists:userPlaylists});
+        }
+        else {
+            res.render('profile', {page_name: 'profile', user: req.user, playlists: ''});
+        }
+      })
 });
 
 module.exports = router;
